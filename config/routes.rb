2f9f499eb57652e71265be07e6ef638a3c234b31
Penek Suksuda <admin@peneksuksuda.com>
2022-08-03 04:26:57 +0700

    Added PDF FUNCTIONALITY

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :decks
   resources :flashcards
   root 'home#index'
+  get 'download' => 'decks#download'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
